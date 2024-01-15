2bb633e5a7c5114fde6facdf74d3c72ab5b10824
abhishek <abhishek.nt94@gmail.com>
2024-01-15 08:57:06 -0800

    Added Nav hov and sticky navbar

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
-document.addEventListener("DOMContentLoaded", function () {
+
+//Navlink Hover Modals
+document.addEventListener('DOMContentLoaded', function () {
+    const modals = document.querySelectorAll('.modal');
     const navLinks = document.querySelectorAll('.nav-link');
 
-    navLinks.forEach(link => {
-        const modalId = link.getAttribute('data-modal');
-        const modal = document.getElementById(modalId);
+    let currentModal = null;
+    let timeout;
+
+    function showAndHideModal(newModal) {
+        clearTimeout(timeout);
+
+        if (currentModal) {
+            currentModal.style.display = 'none';
+        }
+
+        newModal.style.display = 'flex';
+        currentModal = newModal;
+    }
+
+    navLinks.forEach((link, index) => {
+        const modal = modals[index];
 
         link.addEventListener('mouseenter', () => {
-            modal.style.display = 'flex';
+            timeout = setTimeout(() => {
+                showAndHideModal(modal);
+            }, 300);
         });
 
         link.addEventListener('mouseleave', () => {
+            clearTimeout(timeout);
+        });
+    });
+
+    modals.forEach(modal => {
+        modal.addEventListener('mouseenter', () => {
+            clearTimeout(timeout);
+        });
+
+        modal.addEventListener('mouseleave', () => {
             modal.style.display = 'none';
         });
     });
 });
+
+
+//Sticky Navbar
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.querySelector('.navbar-header');
+    var logo = document.getElementById('header-logo');
+    var stickyOffset = navbar.offsetTop;
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > stickyOffset) {
+            navbar.classList.add('sticky');
+            logo.src = "./Assests/srm-logo.svg.svg";
+        } else {
+            navbar.classList.remove('sticky');
+            logo.src = "./Assests/srm-logo-white.svg (1).svg";
+        }
+    });
+});
+
